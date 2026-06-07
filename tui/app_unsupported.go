b4247cee246a9dab10f61f7443a0c22ce79c234a
Pablo Heredia <pablo.heredia@fintoc.com>
2026-06-07 17:56:47 -0400

    tui: add console resizing support via polling on windows

--- tui/app_unsupported.go
+++ tui/app_unsupported.go
@@ -1,4 +1,4 @@
-//go:build !(unix || darwin || linux)
+//go:build !(unix || darwin || linux || windows)
 
 package tui
 
---
+++ tui/app_windows.go
@@ -0,0 +1,42 @@
+//go:build windows
+
+package tui
+
+import (
+	"context"
+	"time"
+)
+
+func (app *app) watchResize(ctx context.Context) {
+	go func() {
+		var lastWidth, lastHeight int
+		if sz, err := app.terminal.Size(); err == nil {
+			lastWidth = sz.Width
+			lastHeight = sz.Height
+		}
+
+		ticker := time.NewTicker(250 * time.Millisecond)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				sz, err := app.terminal.Size()
+				if err != nil {
+					continue
+				}
+				if sz.Width != lastWidth || sz.Height != lastHeight {
+					lastWidth = sz.Width
+					lastHeight = sz.Height
+					select {
+					case app.events <- terminalResized{}:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}
+	}()
+}
